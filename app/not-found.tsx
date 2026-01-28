9b182b6c882e36536709898cc1eccd15800265fb
Ann00012 <ann.malchevska@gmail.com>
2026-01-28 17:03:30 +0200

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,6 @@
-
-'use client';
 import css from "./page.module.css"
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 import { Metadata } from 'next';
+import NotFoundRedirect from "@/components/NotFoundRedirect/NotFoundRedirect";
 
 export const metadata: Metadata = {
   title: "Page not found",
@@ -32,15 +29,9 @@ export const metadata: Metadata = {
 };
 
 const NotFound = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const timer = setTimeout(() => router.push('/'), 3000);
-    return () => clearTimeout(timer);
-  }, [router]);
-
   return (
-    <div className={ css.container}>
+    <div className={css.container}>
+      <NotFoundRedirect />
      <h1 className={css.title}>404 - Page not found</h1>
 <p className={css.descriptionNotFound}>Sorry, the page you are looking for does not exist.</p>
     </div>
